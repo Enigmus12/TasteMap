20de3af61fca2eab836ca31ec78acc08de112f62
Juan <Jdrorodriguez2@gmail.com>
2026-02-17 11:03:05 -0500

    feat: scaffolding

---
+++ restaurants-service/src/test/java/eci/edu/co/restaurants_service/RestaurantsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.co.restaurants_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
