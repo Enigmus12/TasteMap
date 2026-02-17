19e50c7fcf9362d8937c4b3087e4af3c484aa1d7
Juan <Jdrorodriguez2@gmail.com>
2026-02-17 15:32:44 -0500

    feat: scaffolding-users

---
+++ users-service/src/main/java/eci/edu/co/users_service/controller/UserController.java
@@ -0,0 +1,43 @@
+package eci.edu.co.users_service.controller;
+
+import eci.edu.co.users_service.dto.UserRequestDTO;
+import eci.edu.co.users_service.dto.UserResponseDTO;
+import eci.edu.co.users_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    
+    private final UserService userService;
+    
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO createdUser = userService.createUser(userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        List<UserResponseDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable String id) {
+        UserResponseDTO user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ users-service/src/main/java/eci/edu/co/users_service/dto/UserResponseDTO.java
@@ -0,0 +1,18 @@
+package eci.edu.co.users_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String role;
+    private String preferences;
+}
