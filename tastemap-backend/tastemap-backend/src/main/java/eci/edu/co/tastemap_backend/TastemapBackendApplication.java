a52f033ce17bb5002577e58ff2368bbd3cc957cf
Juan <Jdrorodriguez2@gmail.com>
2026-02-17 10:12:04 -0500

    feat: scaffolding

---
+++ tastemap-backend/tastemap-backend/src/main/java/eci/edu/co/tastemap_backend/TastemapBackendApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.co.tastemap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastemapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastemapBackendApplication.class, args);
+	}
+
+}
---
+++ tastemap-backend/tastemap-backend/src/test/java/eci/edu/co/tastemap_backend/TastemapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.co.tastemap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastemapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
